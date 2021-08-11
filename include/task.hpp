10d64923b18adf01f072552beb57034188994ced
Pivnoy <evgenij_makarev20021@mail.ru>
2021-08-11 10:39:51 +0300

    With zero args

--- include/task.hpp
+++ include/task.hpp
@@ -3,12 +3,18 @@
 
 
 class task {
-    template<typename Func, typename ... Args,
-            std::enable_if_t<std::is_invocable_v<Func, Args ...>, bool> = true>
-    constexpr decltype(auto) create_func(Func func, Args ... args) {
-        return func(std::forward<Args>(args)...);
-    }
+
 };
 
+template<typename Func, typename ... Args,
+        std::enable_if_t<std::is_invocable_v<Func, Args ...>, bool> = true>
+constexpr decltype(auto) create_func(Func func, Args ... args) {
+    return func(std::forward<Args>(args)...);
+}
+
+template<typename Func>
+constexpr decltype(auto) create_func(Func func){
+    return func();
+}
 
 #endif //THREADPOOL_CPP_TASK_HPP
