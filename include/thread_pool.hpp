840624d3a3f2dd1fc0b67c4f3aef3c816525c0f8
Kaladin13 <maksim.lagus@yandex.ru>
2021-08-12 14:14:01 +0300

    added initialisation

--- include/thread_pool.hpp
+++ include/thread_pool.hpp
@@ -6,8 +6,16 @@
 
 class thread_pool {
 private:
+    enum{
+        default_thread_num = 4
+    };
+
+    const size_t MAX_NUMBER_OF_THREADS = std::thread::hardware_concurrency();
+    const size_t current_number_of_threads;
+
     std::vector<std::thread> threads;
     std::deque<task> task_queue;
+    std::vector<size_t> empty_threads; // to contain indexes of threads that are ready to run
 
     thread_pool(const thread_pool &) = delete;
     thread_pool & operator=(thread_pool &) = delete;
@@ -18,7 +26,9 @@ private:
 
 public:
 
-    explicit thread_pool();
+    explicit thread_pool(); //default 4 threads
+
+    explicit thread_pool(int number_of_threads); //custom number of threads
 
     virtual ~thread_pool() = default;
 };
--- src/thread_pool.cpp
+++ src/thread_pool.cpp
@@ -1,5 +1,23 @@
 #include "../include/thread_pool.hpp"
 
 
-thread_pool::thread_pool() {}
+thread_pool::thread_pool():current_number_of_threads(default_thread_num){
 
+    for (int i = 0;i<current_number_of_threads;i++)
+        empty_threads.push_back(i); //at start all threads are empty
+
+}
+
+//what to do here if number_of_threads is incorrect value?
+thread_pool::thread_pool(int number_of_threads):current_number_of_threads(number_of_threads) {
+
+    if (!(number_of_threads<=MAX_NUMBER_OF_THREADS || number_of_threads>0)){
+        //handle_error
+    }
+
+    threads.resize(number_of_threads);
+
+    for (int i = 0;i<current_number_of_threads;i++)
+        empty_threads.push_back(i);
+
+}
